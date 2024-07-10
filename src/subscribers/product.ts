83d9c0c31debb6e645f7d7c6a6a4d5b2a90ca849
Miyuru Dharmage <miyuru@10xtek.com>
2024-07-10 05:53:04 +0530

    changes

---
+++ src/subscribers/product.ts
@@ -0,0 +1,32 @@
+import { 
+	ProductService,
+	type SubscriberConfig, 
+	type SubscriberArgs, 
+  } from "@medusajs/medusa";
+import TypesenseService from "../services/typesense";
+  
+  export default async function productUpdateHandler({ 
+	data, eventName, container, pluginOptions, 
+  }: SubscriberArgs<Record<string, any>>) {
+	const productService: ProductService = container.resolve(
+	  "productService"
+	)
+	const typesenseService: TypesenseService = container.resolve(
+	  "typesenseService"
+	)
+  
+	const { id } = data
+  
+	const product = await productService.retrieve(id)
+
+	console.log("Product updated", product)
+  
+	// do something with the product...
+  }
+  
+  export const config: SubscriberConfig = {
+	event: ProductService.Events.UPDATED,
+	context: {
+	  subscriberId: "product-update-typesense-handler",
+	},
+  }
